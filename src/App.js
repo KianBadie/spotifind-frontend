0daf31bb0082db5c9d207926e016ee9fa689c828
Kian Badie <kianbadie@gmail.com>
2022-10-28 11:30:58 -0700

    Add functionality to fetch spotify api token from backend.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Login from './components/Login'
+import { useEffect, useState } from 'react';
+import Login from './components/Login';
+
+import axios from 'axios';
 
 function App() {
+
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const authCode = new URLSearchParams(window.location.search).get('code');
+
+    async function getToken() {
+      const res = await axios.post('auth/token', { code: authCode });
+      const data = res.data;
+      setToken(data.access_token);
+    }
+
+    if(authCode) getToken();
+  }, []);
+
   return (
     <div>
-      <Login/>
+      { token === '' ? <Login/> : token }
     </div>
   );
+
 }
 
 export default App;
