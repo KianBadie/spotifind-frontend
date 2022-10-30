89226d1a656a9bf9dd92a1ddebc7c19519b1b927
Kian Badie <kianbadie@gmail.com>
2022-10-30 15:53:29 -0700

    Remove functionality in search and move token fetching to App.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
 import Login from './components/Login';
 import Search from './components/Search';
 
+import axios from 'axios';
+
 function App() {
 
-  const authCode = new URLSearchParams(window.location.search).get('code');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const authCode = new URLSearchParams(window.location.search).get('code');
+
+    async function getToken() {
+      const res = await axios.post('auth/token', { code: authCode });
+      const data = res.data;
+      setToken(data.access_token);
+    }
+
+    if(authCode) getToken();
+  });
 
   return (
     <div>
-      { !authCode ? <Login/> : <Search authCode={authCode} /> }
+      { !token ? <Login/> : <Search token={token} /> }
     </div>
   );
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,7 @@
-import { useState, useEffect } from 'react';
-
-import axios from 'axios';
-
 function Search(props) {
-
-    const [token, setToken] = useState('');
-
-    useEffect(() => {
-        async function getToken() {
-            const res = await axios.post('auth/token', { code: props.authCode });
-            const data = res.data;
-            setToken(data.access_token);
-        }
-
-        getToken();
-    }, []);
-
     return (
         <div>
-            Your token is {token}.
+            {props.token}.
         </div>
     );
 
