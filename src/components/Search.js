bd2a070f3faef8400c8cfeec59ab9ee8c3b6d2df
Kian Badie <kianbadie@gmail.com>
2022-10-30 16:20:38 -0700

    Add functionality to fetch playlist in search component.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
 function Search(props) {
+
+    const [playlist, setPlaylist] = useState([]);
+
+    useEffect(() => {
+        async function getPlaylist() {
+            const res = await axios.get('https://api.spotify.com/v1/me/playlists', { headers: { 'Authorization': `Bearer ${props.token}` } });
+            const data = res.data;
+            setPlaylist(data.items);
+        }
+
+        getPlaylist();
+    }, []);
+
     return (
         <div>
-            {props.token}.
+            Search component.
         </div>
     );
 
