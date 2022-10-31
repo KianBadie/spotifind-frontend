6de702641710a1b94c10d56c1d868a42d9239147
Kian Badie <kianbadie@gmail.com>
2022-10-30 18:08:53 -0700

    Add functionality to fetch and create track docs from playlists for searching.

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,16 +4,46 @@ import axios from 'axios';
 
 function Search(props) {
 
-    const [playlist, setPlaylist] = useState([]);
+    async function getPlaylist(token) {
+        const res = await axios.get('https://api.spotify.com/v1/me/playlists', { headers: { 'Authorization': `Bearer ${token}` } });
+        const data = res.data;
+        return data.items;
+    }
+
+    async function getPlaylistItems(token, playlist) {
+        const tracksUrl = playlist.tracks.href;
+        const res = await axios.get(tracksUrl, { headers: { 'Authorization': `Bearer ${token}` } });
+        const data = res.data;
+        return data.items;
+    }
+
+    function getPlaylistTrackDocs(playlist, playlistItems) {
+        const trackDocs = playlistItems.map(playlistItem => {
+            const track = playlistItem.track;
+            const artist = track.artists.map(artist => artist.name);
+            return {
+                name: track.name,
+                album: track.album.name,
+                artist: artist,
+                playlist: playlist.name
+            }
+        });
+        return trackDocs;
+    }
 
     useEffect(() => {
-        async function getPlaylist() {
-            const res = await axios.get('https://api.spotify.com/v1/me/playlists', { headers: { 'Authorization': `Bearer ${props.token}` } });
-            const data = res.data;
-            setPlaylist(data.items);
+        async function initializeIndex() {
+            const playlists = await getPlaylist(props.token);
+            const trackDocs = [];
+            for(const playlist of playlists) {
+                const playlistItems = await getPlaylistItems(props.token, playlist);
+                const playlistTrackDocs = getPlaylistTrackDocs(playlist, playlistItems);
+                trackDocs.push(...playlistTrackDocs);
+            }
+            console.log(trackDocs);
         }
 
-        getPlaylist();
+        initializeIndex();
     }, []);
 
     return (
