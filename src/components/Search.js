fa5571bec751c405540e68b492c1c1138f55e4ee
Kian Badie <kianbadie@gmail.com>
2022-10-31 11:15:16 -0700

    Create var for documentDict in Search mounting function.

--- src/components/Search.js
+++ src/components/Search.js
@@ -30,11 +30,12 @@ function Search(props) {
             const playlists = await getPlaylist(props.token);
             const playlistsItems = await Promise.all(playlists.map(playlist => getPlaylistItems(props.token, playlist.id)));
             const documents = playlists.flatMap((playlist, i) => getPlaylistItemDocuments(playlist, playlistsItems[i]));
-
-            setDocumentDict(documents.reduce((dict, document) => {
+            const documentDict = documents.reduce((dict, document) => {
                 dict[document.id] = document;
                 return dict;
-            }, {}));
+            }, {});
+
+            setDocumentDict(documentDict);
 
             const index = lunr(function() {
                 this.ref('id');
