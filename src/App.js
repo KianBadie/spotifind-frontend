281a03c5decd6fc760f5a4c395baae248f35424b
Kian Badie <kianbadie@gmail.com>
2022-11-03 08:48:26 -0700

    Fix tab spacing in App.

--- src/App.js
+++ src/App.js
@@ -7,41 +7,41 @@ import axios from 'axios';
 
 function App() {
 
-  const [token, setToken] = useState('');
-  const [failedAuth, setFailedAuth] = useState(false);
+	const [token, setToken] = useState('');
+	const [failedAuth, setFailedAuth] = useState(false);
 
-  const authCode = new URLSearchParams(window.location.search).get('code');
+	const authCode = new URLSearchParams(window.location.search).get('code');
 
-  const renderApp = () => {
-    if(token) {
-      return <Spotifind token={token}/>;
-    } else if (authCode && !failedAuth) {
-      return null;
-    } else {
-      return <Welcome failedAuth={failedAuth}/>;
-    }
-  };
+	const renderApp = () => {
+		if(token) {
+			return <Spotifind token={token}/>;
+		} else if (authCode && !failedAuth) {
+			return null;
+		} else {
+			return <Welcome failedAuth={failedAuth}/>;
+		}
+	};
 
-  useEffect(() => {
-    if(!authCode) return;
+	useEffect(() => {
+		if(!authCode) return;
 
-    setFailedAuth(false);
+		setFailedAuth(false);
 
-    axios.post('auth/token', { code: authCode })
-      .then((res) => {
-        setToken(res.data.access_token);
-      })
-      .catch((err) => {
-        setFailedAuth(true);
-      });
-  }, [authCode]);
+		axios.post('auth/token', { code: authCode })
+			.then((res) => {
+				setToken(res.data.access_token);
+			})
+			.catch((err) => {
+				setFailedAuth(true);
+			});
+	}, [authCode]);
 
-  return (
-    <div>
-      <Header/>
-      {renderApp()}
-    </div>
-  );
+	return (
+		<div>
+			<Header/>
+			{renderApp()}
+		</div>
+	);
 
 }
 
