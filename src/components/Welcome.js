ca27c28f2d8323c4f73f56a96df0d64f597824a6
Kian Badie <kianbadie@gmail.com>
2022-11-03 09:05:17 -0700

    Modify Welcome page log in button and add app description.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,13 @@
 function Welcome(props) {
     return (
         <div>
+            <div>
+                <p>Search for songs, artists, and albums across your entire playlists library.</p>
+            </div>
             {props.failedAuth && 
                 <p>Authorization failed. Please login again.</p>
             }
-            <a href="/auth/login">Login</a>
+            <a href="/auth/login">LOG IN WITH SPOTIFY</a>
         </div>
     );
 }
