734f7dfd2d732bed36bee7225124a090ecd447cf
Kian Badie <kianbadie@gmail.com>
2022-11-03 13:35:32 -0700

    Add skew style to DemoGif.

--- src/components/DemoGif.js
+++ src/components/DemoGif.js
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 const DemoGifImg = styled.img`
     max-width: 100%;
     max-height: 100%;
+    border-radius: 16px;
     display: block;
     object-fit: cover;
+    box-shadow: 0px 0px 6px #00000026;
+    transform: perspective(1000px) rotateY(350deg);
 `
 
 function DemoGif() {
