a318030d871d1ebbfad00c4ae9371fbff2cd7bdb
Kian Badie <kianbadie@gmail.com>
2022-11-04 11:54:05 -0700

    Improve display of Welcome on mobile.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,27 +3,46 @@ import LoginButton from './LoginButton';
 import DemoGif from './DemoGif';
 
 import styled from 'styled-components';
+import breakpoints from '../breakpoints';
 
 const WelcomeContainer = styled.div`
     height: 100vh;
     display: flex;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
+
+    @media ${breakpoints.desktop} {
+        flex-direction: row;
+    }
 `;
 
 const DescriptionContainer = styled.div`
-    flex: 0 0 40%;
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
+    align-items: center;
     color: #5c5c5c;
+
+    @media ${breakpoints.desktop} {
+        flex: 0 0 40%;
+        align-items: flex-start;
+    }
 `;
 
 const WelcomeSiteTitle = styled(SiteTitle)`
-    margin-bottom: 32px;
+    margin-bottom: 16px;
+
+    @media ${breakpoints.desktop} {
+        margin-bottom: 32px;
+    }
 `;
 
 const Description = styled.p`
-    margin: 0 0 64px 0;
+    margin: 0 0 32px 0;
+
+    @media ${breakpoints.desktop} {
+        margin: 0 0 64px 0;
+    }
 `;
 
 function Welcome(props) {
