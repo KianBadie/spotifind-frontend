4cd16534d3b1a3fa8355663afb2c34ae5d5d336f
Kian Badie <kianbadie@gmail.com>
2022-11-04 11:30:55 -0700

    Refactor and remove unecessary styles in Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,43 +7,34 @@ import styled from 'styled-components';
 const WelcomeContainer = styled.div`
     height: 100vh;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
     align-items: center;
-`;
-
-const WelcomeContent = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    gap: 32px;
 `;
 
 const DescriptionContainer = styled.div`
     flex: 0 0 40%;
-    height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: space-evenly;
     align-items: flex-start;
     color: #5c5c5c;
 `;
 
+const WelcomeSiteTitle = styled(SiteTitle)`
+    margin-bottom: 32px;
+`;
+
 const Description = styled.p`
-    margin: 0;
+    margin: 0 0 64px 0;
 `;
 
 function Welcome(props) {
     return (
         <WelcomeContainer className={props.className}>
-            <WelcomeContent>
-                <DescriptionContainer>
-                    <SiteTitle/>
-                    <Description>Search for songs, artists, and albums across your entire playlists library.</Description>
-                    <LoginButton/>
-                </DescriptionContainer>
-                <DemoGif/>
-            </WelcomeContent>
+            <DescriptionContainer>
+                <WelcomeSiteTitle/>
+                <Description>Search for songs, artists, and albums across your entire playlists library.</Description>
+                <LoginButton/>
+            </DescriptionContainer>
+            <DemoGif/>
         </WelcomeContainer>
     );
 }
