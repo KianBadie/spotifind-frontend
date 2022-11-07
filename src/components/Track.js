e0828d6821b96cf667121c9bfa8b63d30d0ef44e
Kian Badie <kianbadie@gmail.com>
2022-11-07 13:48:11 -0800

    Add track info and stylings to Track.

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,12 +1,51 @@
+import styled from "styled-components";
+
+const TrackContainer = styled.div`
+    display: flex;
+    align-items: center;
+    color: #5c5c5c;
+`;
+
+const AlbumCover = styled.img`
+    margin-right: 16px;
+`;
+
+const TitleContainer = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+`;
+
+const AlbumContainer = styled.div`
+    flex: 1;
+`;
+
+const PlaylistContainer = styled.div`
+    flex: 1;
+`;
+
 function Track(props) {
 
     const track = props.track;
+    const artist = track.artists[0];
+    const album = track.album;
     const playlist = track.playlist;
 
     return (
-        <div className={props.className}>
-            {track.name} : {track.album.name} - {playlist.name}
-        </div>
+        <TrackContainer className={props.className}>
+            <AlbumCover src={playlist.images[2].url}/>
+            <TitleContainer>
+                <span>{track.name}</span>
+                <span>{artist.name}</span>
+            </TitleContainer>
+            <AlbumContainer>
+                <span>{album.name}</span>
+            </AlbumContainer>
+            <PlaylistContainer>
+                <span>{playlist.name}</span>
+            </PlaylistContainer>
+        </TrackContainer>
     );
 
 }
