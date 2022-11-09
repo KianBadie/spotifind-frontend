77c251161d12546ecc7776ff761efead4fc461ad
Kian Badie <kianbadie@gmail.com>
2022-11-09 11:00:17 -0800

    Modify playlist item fetching to account for pagination.

--- src/spotifyUtils.js
+++ src/spotifyUtils.js
@@ -25,7 +25,25 @@ export async function getPlaylist(token) {
 }
 
 export async function getPlaylistItems(token, id) {
-    const res = await axios.get(`https://api.spotify.com/v1/playlists/${id}/tracks`, { headers: { 'Authorization': `Bearer ${token}` } });
-    const data = res.data;
-    return data.items;
+
+    const items = [];
+
+    const headers = {
+        'Authorization': `Bearer ${token}`
+    };
+
+    const params = {
+        limit: 50
+    }
+
+    let next = `https://api.spotify.com/v1/playlists/${id}/tracks`;
+
+    while(next) {
+        const res = await axios.get(next, { headers, params });
+        const data = res.data;
+        next = data.next;
+        items.push( ...data.items );
+    }
+
+    return items;
 }
