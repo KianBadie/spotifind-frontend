1a533adbdc5e6ec98baafa7a7f98444c1d7708ab
Kian Badie <kianbadie@gmail.com>
2022-11-17 10:49:38 -0800

    Add social icons and config file to control social links.

---
+++ src/socials.js
@@ -0,0 +1,14 @@
+const socials = [
+    {
+        name: 'GitHub',
+        link: 'https://github.com/KianBadie/trulyfound-frontend',
+        icon: 'social/GitHub-Mark-32px.png'
+    },
+    {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/kian-badie',
+        icon: 'social/LI-In-Bug.png'
+    }
+];
+
+export default socials;
